3176134239843803ebf42d523ae590edbdf786d0
wenlianghuang <wenliangmatt@gmail.com>
2022-10-01 16:12:16 +0800

    modified file

--- cd.bat
+++ cd.bat
@@ -3,9 +3,6 @@ Rem The cd withut any parameters is used to display the current working director
 cd 
 REM Test
 REM Test2
-REM Test3
-REM Test4
-REM Test5
 cd /d %USERPROFILE% 
 Rem echo %USERPROFILE%
 cd /d "D:\python_practice"
