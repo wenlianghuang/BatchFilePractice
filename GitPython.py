e9e99daa14719c4b340d320ad411c2dc3d0c17ad
wenlianghuang <wenliangmatt@gmail.com>
2022-10-01 12:33:53 +0800

    test

--- GitPython.py
+++ GitPython.py
@@ -2,6 +2,7 @@ import git
 with git.Repo.init(path='.') as repo:
     #if len(repo.untracked_files) != 0:
     #    untracked_files = repo.untracked_files
+    #
     changedFile = [item.a_path for item in repo.index.diff(None)]
     if len(changedFile) == 0:
         print("The codes in git are all latest")
