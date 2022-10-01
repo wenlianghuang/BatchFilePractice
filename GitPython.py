713bcf7a8322dc45208c60182205f539af275fb6
wenlianghuang <wenliangmatt@gmail.com>
2022-10-01 14:50:40 +0800

    modified file

--- GitPython.py
+++ GitPython.py
@@ -3,6 +3,10 @@ with git.Repo.init(path='.') as repo:
     #if len(repo.untracked_files) != 0:
     #    untracked_files = repo.untracked_files
     #
+    
+    #Have to add the passwoard at first?
+    #1.eval `ssh-agent -s`
+    #2.ssh-add ~/.ssh/id_rsa
     changedFile = [item.a_path for item in repo.index.diff(None)]
     if len(changedFile) == 0:
         print("The codes in git are all latest")
