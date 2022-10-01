762ed68ec63fe0be1c516b08972dd3df78d40d1d
wenlianghuang <wenliangmatt@gmail.com>
2022-10-01 10:38:00 +0800

    modified file

--- cd.bat
+++ cd.bat
@@ -1,7 +1,7 @@
 @echo off
 Rem The cd withut any parameters is used to display the current working directory
 cd 
-
+REM Test
 REM Changing the path to Program Files
 cd /d %USERPROFILE% 
 Rem echo %USERPROFILE%
