73d96421f72b15e885b46eee9a7516a53a0b39d9
wenlianghuang <wenliangmatt@gmail.com>
2022-10-01 11:24:34 +0800

    modified file

--- cd.bat
+++ cd.bat
@@ -2,7 +2,6 @@
 Rem The cd withut any parameters is used to display the current working directory
 cd 
 REM Test
-REM Changing the path to Program Files
 cd /d %USERPROFILE% 
 Rem echo %USERPROFILE%
 cd /d "D:\python_practice"
