0829fed11b9cad751323ed838de72d02ef2b80da
wenlianghuang <wenliangmatt@gmail.com>
2022-10-03 22:11:57 +0800

    modified file

--- GitPython.py
+++ GitPython.py
@@ -1,15 +1,20 @@
+#Have to add the passwoard at first?(linux/Mac)
+#1.eval `ssh-agent -s`
+#2.ssh-add ~/.ssh/id_rsa
+    
+#Have to add the password at first?(Windows)
+#1.C:\Program Files\Git\cmd\start-ssh-agent.cmd
 import git
 with git.Repo.init(path='.') as repo:
-    #if len(repo.untracked_files) != 0:
-    #    untracked_files = repo.untracked_files
-    #
+    if len(repo.untracked_files) != 0:
+        untracked_files = repo.untracked_files
+        for i in range(len(untracked_files)):
+            repo.index.add(items=[untracked_files[i]])
+            repo.index.commit("new file")
+        print('All the new file are committed')
+    else:
+        print('There is no untracted file')
     
-    #Have to add the passwoard at first?(linux/Mac)
-    #1.eval `ssh-agent -s`
-    #2.ssh-add ~/.ssh/id_rsa
-    
-    #Have to add the password at first?(Windows)
-    #1.C:\Program Files\Git\cmd\start-ssh-agent.cmd
     
     
     changedFile = [item.a_path for item in repo.index.diff(None)]
